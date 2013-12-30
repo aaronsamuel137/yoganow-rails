7666973de6e99ad6184dfd73f3fa1d86a16a17fe
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2013-12-30 15:06:02 -0700

    add events page

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,5 @@
+class EventsController < ApplicationController
+  def events()
+    gon.hello = 1
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Yoga::Application.routes.draw do
   root 'listing#index'
   get '/api', to: 'api#index'
+  get '/events', to: 'events#events'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
